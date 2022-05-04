21f37a689bb295560266f3b69c0a8e2b9d0a859f
VasilinaFreedom <vasilinamkrvch@gmail.com>
2022-05-04 14:37:10 +0300

    create: add keyCode for key

---
+++ Virtual Keyboard/index.js
@@ -0,0 +1,22 @@
+// let div = document.createElement('div');
+//   div.className = "text";
+//   div.innerHTML = "<h1>Hello, Nik!</h1>";
+//   document.body.append(div);
+
+//   let div2=div.cloneNode(true)
+
+//   div2.innerHTML = "<h1>Hello, Vas!</h1>";
+// //   document.body.append(div);
+// div.after(div2)
+
+let div = document.createElement('div');
+div.className = "keyboard";
+document.body.append(div);
+
+let keyboard=[49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47]
+document.onkeypress = function(event){
+    // console.log(event)
+    keyboard.push(event.keyCode)
+    console.log(keyboard) //просмотр кодов клавиш (коды цифр и текста в массив )
+}
+
